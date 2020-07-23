b2b798cda076af8aaac956201b8f88a04367561f
hsiehkl <st4465@gmail.com>
2020-07-24 02:16:17 +0800

    Add api_connector.py

---
+++ api_connector.py
@@ -0,0 +1,42 @@
+import gspread
+import pandas as pd
+import json
+
+def read_gsheet(sheet_name):
+    gc = gspread.service_account(filename="cred/credentials.json")
+    # read json
+    with open("google_sheets.json", 'r') as google_sheets:
+        sheets = google_sheets.read()
+    # parse url
+    url = json.loads(sheets)[sheet_name]
+    sh = gc.open_by_url(url)
+    worksheet = sh.get_worksheet(0)
+    return worksheet
+
+def update(worksheet, name_list, date):
+
+    list_of_lists = worksheet.get_all_values()
+    headers = list_of_lists[0]
+    df = pd.DataFrame(list_of_lists[1:], columns=headers)
+
+    row = None
+    column = None
+
+    try:
+        column = df.columns.get_loc(date) + 1
+    except:
+        column = df.shape[1] + 1
+        worksheet.update_cell(1, column, date)
+
+    for name in name_list:
+
+        if name in df['Name'].values:
+            row = df[df['Name']==name].index[0] + 2
+            worksheet.update_cell(row, column, 'O')
+        else:
+            print('We cannot find this person in the attendance list.')
+            
+def update_attendence(sheet_name, name_list, date):
+    worksheet = read_gsheet(sheet_name)
+    update(worksheet, name_list, date)
+    print('Finish updating attendence.')
