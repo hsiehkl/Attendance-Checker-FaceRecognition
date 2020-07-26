61884a08956e518723e7624c4ba0726c52ce5917
hsiehkl <st4465@gmail.com>
2020-07-26 15:16:19 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,28 +1,32 @@
 import face_recognition
-# import dlib
+import dlib
 from sklearn import svm
 import os
 import pickle
 
+def listdir(path):
+    return [f for f in os.listdir(path) if not f.startswith('.')]
 
-def train(model_name):
+def train(folder, model_name):
 
     # The training data would be all the face encodings from all the known images and the labels are their names
     encodings = []
     names = []
 
+    # Root path
+    root_folder = 'train_dir/' + folder + '/'
+
     # Training directory
-    train_dir = os.listdir('train_dir')
+    train_dir = listdir(root_folder)
 
     # Loop through each person in the training directory
     for person in train_dir:
-
-        pix = os.listdir('train_dir/' + person)
+        pix = listdir(root_folder + person)
 
         # Loop through each training image for the current person
         for person_img in pix:
             # Get the face encodings for the face in each image file
-            face = face_recognition.load_image_file('train_dir/' + person + "/" + person_img)
+            face = face_recognition.load_image_file(root_folder + person + "/" + person_img)
             face_bounding_boxes = face_recognition.face_locations(face)
 
             #If training image contains exactly one face
@@ -34,7 +38,6 @@ def train(model_name):
             else:
                 print(person + "/" + person_img + " was skipped and can't be used for training")
 
-
     # Create and train the SVC classifier
     clf = svm.SVC(gamma='scale')
     clf.fit(encodings, names)
@@ -42,3 +45,4 @@ def train(model_name):
     # Save classifier
     pickle.dump(clf, open('models/' + '{}.pkl'.format(model_name), 'wb'))
 
+#train('BIPM2019-bigdata', 'BIPM2019_bigdata')
