a95abc80a6247a6ade2e19b3c72ce43f7617ac99
hsiehkl <st4465@gmail.com>
2020-07-26 18:58:45 +0800

    Add run.py

---
+++ run.py
@@ -0,0 +1,23 @@
+from api_connector import update_attendence
+from train import train
+from predict import predict_video
+import os
+
+
+def main(train_path, media, sheet, date):
+
+    models = [f for f in os.listdir('models') if not f.startswith('.')]
+    if sheet+'.pkl' not in models:
+        print('start training model for {}.'.format(sheet))
+        train(train_path, sheet)
+    else:
+        print("{} model has already existed.".format(sheet))
+
+    attendance_list  = predict(media_path, sheet+'.pkl')
+    update_attendence(sheet, attendance_list, date)
+
+
+    
+main('1', '1', '1')
+# if __name__ == "__main__":
+#     import plac; plac.call(main)
